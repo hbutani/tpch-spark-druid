a2a5aea36169aa49e0bb65add754b97f79bf7761
Harish Butani <hbutani@hortonworks.com>
2016-01-12 11:57:54 -0800

    TpchGenMain for local mode

--- tpchData/src/main/scala/org/sparklinedata/tpch/TpchGenMain.scala
+++ tpchData/src/main/scala/org/sparklinedata/tpch/TpchGenMain.scala
@@ -1,5 +1,6 @@
 package org.sparklinedata.tpch
 
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{DataFrame, SQLContext}
 
@@ -99,5 +100,50 @@ class TpchGenMain(sqlCtx : SQLContext, base: String, scale : String) {
     ol.show(100)
   }
 
+
+}
+
+case class Config(baseDir : String = "", scale : String = "")
+
+/**
+ * Program to create a flattened tpch dataset using spark-submit. Sample Usage:
+  * {{{
+  *   bin/spark-submit --packages com.databricks:spark-csv_2.10:1.1.0,SparklineData:spark-datetime:0.0.2 \
+  *   --jars /Users/hbutani/sparkline/spark-druid-olap/target/scala-2.10/spark-druid-olap-assembly-0.0.1.jar \
+  *   --class org.sparklinedata.tpch.TpchGenMain \
+*   /Users/hbutani/sparkline/tpch-spark-druid/tpchData/target/scala-2.10/tpchdata-assembly-0.0.1.jar \
+  *   --baseDir /Users/hbutani/tpch/ --scale 1
+  * }}}
+ *
+ */
+object TpchGenMain {
+
+  def main(args: Array[String]): Unit = {
+
+    val parser = new scopt.OptionParser[Config]("tpchGenFlat") {
+      head("tpchGenFlat", "0.1")
+      opt[String]('d', "baseDir") required() valueName("<dir>") action { (x, c) =>
+        c.copy(baseDir = x) } text("the base dir for tpch data")
+      opt[Int]('s', "scale") required() valueName("<dataScale>") action { (x, c) =>
+        c.copy(scale = s"datascale$x") } text("the scale of data(1/10/100/...)")
+      help("help") text("prints this usage text")
+    }
+
+    val c = parser.parse(args, Config()) match {
+      case Some(config) => config
+
+      case None => sys.exit(-1)
+    }
+
+    val sc = new SparkContext(new SparkConf().setAppName("TPCHGen Flattened Data"))
+
+    val sqlCtx = new HiveContext(sc)
+
+    val tpchGenMain = new TpchGenMain(sqlCtx,
+      c.baseDir,
+      c.scale)
+
+    tpchGenMain.createDenormalizedData
+  }
 
 }
